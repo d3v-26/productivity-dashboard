c12cd2d9c761ab0641f54eeda71ebbdde0fd2eb1
Dev Patel <68773259+d3v-26@users.noreply.github.com>
2026-03-08 16:06:48 -0400

    Fix base path for productivity-dashboard   repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/pl/',
+  base: '/productivity-dashboard/',
   plugins: [
     react(),
     tailwindcss(),
